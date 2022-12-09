3bc62c1694cc60b6af8467130fe452f3b562ca8c
samgh <samgharama@outlook.com>
2022-12-09 23:35:48 +0800

    fix coc.lua keymaps error

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -15,11 +15,11 @@ M.setup = function()
     }
 
     local keymaps = {
-        { "i", "<tab>", "coc#pum#visible() ? coc#pum#next(2) : col('.') == 1 || getline('.')[col('.') - 2] =~# '\\s' ? '\\<tab>' : coc#refresh()",
+        { "i", "<tab>", "coc#pum#visible() ? coc#pum#next(1) : col('.') == 1 || getline('.')[col('.') - 2] =~# '\\s' ? \"\\<tab>\" : coc#refresh()",
             { silent = true, noremap = true, expr = true, replace_keycodes = false } },
-        { "i", "<s-tab>", "coc#pum#visible() ? coc#pum#prev(2) : '\\<c-d>'",
+        { "i", "<s-tab>", "coc#pum#visible() ? coc#pum#prev(1) : \"\\<c-d>\"",
             { silent = true, noremap = true, expr = true, replace_keycodes = false } },
-        { "i", "<cr>", "coc#pum#visible() ? coc#_select_confirm() : '\\<c-g>u\\<cr>\\<c-r>=coc#on_enter()\\<cr>'",
+        { "i", "<cr>", "coc#pum#visible() ? coc#_select_confirm() : \"\\<c-g>u\\<cr>\\<c-r>=coc#on_enter()\\<cr>\"",
             { silent = true, noremap = true, expr = true, replace_keycodes = false } },
 
         { "n", "gd", "<Plug>(coc-definition)",      { silent = true } },
